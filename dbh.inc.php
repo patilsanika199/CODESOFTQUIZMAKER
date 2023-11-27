d2b3b4642a863c2fc5e11c581158deda22aefdcb
sanika patil <120210186+patilsanika199@users.noreply.github.com>
2023-11-27 21:37:58 +0530

    Add files via upload
    Signed-off-by: sanika patil <120210186+patilsanika199@users.noreply.github.com>

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "id6355594_abc";
+$dbPassword = "abc123";
+$dbName = "id6355594_quiz";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
