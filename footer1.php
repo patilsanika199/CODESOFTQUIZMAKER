a165b404ad2490e512922cbd15a11677834d59fd
sanika patil <120210186+patilsanika199@users.noreply.github.com>
2023-11-27 21:41:10 +0530

    Add files via upload
    Signed-off-by: sanika patil <120210186+patilsanika199@users.noreply.github.com>

---
+++ footer1.php
@@ -0,0 +1,22 @@
+<footer>
+		<div class="last">
+			<div class="footer1">
+				<span><b> Follow Us :</b> </span>&nbsp;
+				<a href="#" class="fa fa-facebook"></a>&nbsp;&nbsp;&nbsp;
+				<a href="#" class="fa fa-instagram"></a>
+			</div>
+
+			<div class="footer2">
+				<p>Just for FUN</p>
+			
+			</div>
+
+			<div class="footer3">
+				<p>Made by <b>SANIKA PATIL</b></p>
+			</div>
+		</div>
+</footer>	
+
+
+</body>
+</html>
